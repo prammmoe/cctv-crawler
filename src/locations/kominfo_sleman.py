f1479df87bb33ff785aea6c8eb543348f0329067
prammmoe <ikhwanpramuditha05@gmail.com>
2024-06-05 22:42:38 +0700

    refactor location to class

---
+++ src/locations/kominfo_sleman.py
@@ -0,0 +1,61 @@
+import cv2
+import os
+import time
+
+
+class KominfoSlemanCCTV:
+    def __init__(self):
+        self.counter = 1
+        self.capturing = True
+        self.location = [
+
+        ]   
+        self.count_pic = 0 
+
+    def choose_location(self):
+        for idx, loc in enumerate(self.location):
+            print(f"{idx + 1}. {loc}")
+        i = input("Pilih lokasi: ")
+        self.selected_location = int(i) - 1
+
+    def get_count_pic(self):
+        self.count_pic = int(input("Jumlah gambar: "))
+    
+    def capture_images(self):
+        print("Opening stream ...")
+        while self.capturing:
+            vcap = cv2.VideoCapture(f"https://mam.jogjaprov.go.id:1937/cctv-kominfosleman/{self.location[self.selected_location]}.stream/playlist.m3u8")
+
+            if vcap.isOpened():
+                print("Collected")
+
+                # Create dirs
+                directory = self.location[self.selected_location]
+                complete_dir = os.path.join('images', directory)
+                
+                if not os.path.exists(complete_dir):
+                    os.makedirs(complete_dir)
+
+                ret, frame = vcap.read()
+
+                if ret:
+                    filename = os.path.join(complete_dir, f"{time.time()}.jpg")
+                    cv2.imwrite(filename, frame)
+                    print(f"Image: {filename}")
+
+                    start = time.time()
+                    while not ((time.time() - start) > 10): # Capture delay min 10 seconds to avoid duplicate images and to increase datasets variance
+                        pass
+
+                    self.counter += 1
+                    if self.counter > self.count_pic:
+                        self.capturing = False
+
+                vcap.release()
+        
+        print(f"Finish, successfully collected {self.count_pic} images")
+
+    def run(self):
+        self.choose_location()
+        self.get_count_pic()
+        self.capture_images()
